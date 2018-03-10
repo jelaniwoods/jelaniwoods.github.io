464d418e1645505607a2d9f10dcfaeea8e9abe53
Windows Letgoner <jmw686@nyu.edu>
2018-03-10 16:08:26 -0600

    modal displays responsive

--- assets/flavor.js
+++ assets/flavor.js
@@ -22,10 +22,10 @@
 });
   }(jQuery));
 
-  function ScrollTo(name) {
-    // console.log(name + ' ddij' + document.getElementById(name));
-    ScrollToResolver(document.getElementById(name));
-  }
+function ScrollTo(name) {
+  // console.log(name + ' ddij' + document.getElementById(name));
+  ScrollToResolver(document.getElementById(name));
+}
 function ScrollToResolver(elem) {
   // console.log(elem + ' fiuh');
   var jump = parseInt(elem.getBoundingClientRect().top * .25);
@@ -37,4 +37,33 @@ function ScrollToResolver(elem) {
   } else {
     elem.lastjump = null;
   }
+}
+function handleResize() {
+  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var lotus = document.getElementById('lotus_link');
+  var hy = document.getElementById('hy_link');
+  var ama = document.getElementById('ama_link');
+  var p = document.getElementById('p_link');
+  if(w <= 979) {
+    lotus.href = 'https://www.philippanormanmd.com';
+    hy.href = 'https://www.hannahyehudah.com';
+    ama.href = 'https://alainamichaelart.tumblr.com/';
+    p.href = '/productivity';
+    var cards = document.getElementsByClassName('card');
+    console.log(cards + ' ddd ' + cards[0]);
+    for (var i = 0; i < 4; i++) {
+      console.log(' ff ' + cards[i]);
+      // if (card.hasOwnProperty(key)) {
+      cards[i].removeAttribute('data-toggle');
+        
+      // }
+    }
+  }
+  else {
+    lotus.removeAttribute("href");
+    hy.removeAttribute("href");
+    ama.removeAttribute("href");
+    p.removeAttribute("href");
+  }
+
 }

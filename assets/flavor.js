255300dcd31e3238efa84fb98858003bbaf08b0a
Windows Letgoner <jmw686@nyu.edu>
2018-03-10 13:13:16 -0600

    change reveal height

--- assets/flavor.js
+++ assets/flavor.js
@@ -9,7 +9,7 @@
 	$(function () {
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 100) {
+			if ($(this).scrollTop() > 200) {
 				$('.navbar').fadeIn();
 			} else {
 				$('.navbar').fadeOut();
